698ffd51e6f8d175c51f197064c6079948619c33
Sang Doan <std35@georgetown.edu>
2024-09-30 22:51:12 -0700

    Create page.tsx

---
+++ app/now/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+    return (
+        <section>
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                What Now
+            </h1>
+            <p className="mb-4">
+                I'm currently working on a new project.
+            </p>
+            <div className="my-16" />
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                About
+            </h1>
+            <p className="mb-4">
+                {`I'm currently a software engineer at a startup.`}
+            </p>
+        </section>
+    )
+}
