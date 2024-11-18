28ea8ad59e46402ecc822dac692912b3289c377b
Sang Doan <std35@georgetown.edu>
2024-11-18 00:02:27 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,7 @@ export default function Page() {
         Sang Doan
       </h1>
       <p className="mb-4">
-        {`I grew up in a small town in Nam Dinh, Vietnam, and later found my way to Hanoi for high school. 
-        That journey eventually led me to Georgetown University, 
-        where I discovered my disdain for academia and career workshops. 
-        Now, a few months past graduation, I’m in California, working on consumer AI projects and figuring it out as I go.`}
+        {`Currently life mostly consists of coding, calligraphy and CoDM. Based in Bay Area, working on consumer AI projects and figuring it out as I go.`}
       </p>
       <p className="text-neutral-600 dark:text-neutral-400">
         {`I offer full-stack development services to founders and startups. See my `}
